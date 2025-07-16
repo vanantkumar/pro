0cd312e3524b158a1e575131f936a76c6eec795e
vanantkumar <vanantkumar09@gmail.com>
2025-07-16 20:27:51 +0530

    Update email_agent.py

--- email_agent.py
+++ email_agent.py
@@ -13,7 +13,7 @@ Email:
 Reply:
 """
     response = client.chat.completions.create(
-        model="Phind-70B",
+        model="gpt-4",
         messages=[{"role": "user", "content": prompt}]
     )
     return response.choices[0].message.content
